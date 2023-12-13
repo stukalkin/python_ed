0fd8c87b977355e4febcdd8dc843d77ae7104119
romanstukalkin <stukalkinrv@yandex.ru>
2023-12-13 22:26:05 +0400

    commit

--- main.py
+++ main.py
@@ -474,3 +474,73 @@ def rev(n):
 rev(2)
 
 
+"""
+Прочесть с помощью pandas файл
+california_housing_test.csv, который находится в папке
+sample_data
+2. Посмотреть сколько в нем строк и столбцов
+3. Определить какой тип данных имеют столбцы
+"""
+
+from pandas import read_csv
+
+data = read_csv('california_housing_test.csv')
+print(type(data))
+print(data.shape)
+print(data.dtypes)
+print(data.info())
+print(data.describe())
+
+
+
+
+'''
+Задача №59. Решение в группах
+1. Проверить есть ли в файле пустые значения
+2. Показать median_house_value где median_income < 2
+3. Показать данные в первых 2 столбцах
+4. Выбрать данные где housing_median_age < 20 и
+median_house_value > 70000
+'''
+
+from pandas import read_csv
+data = read_csv('california_housing_test.csv')
+# # 1. Проверить есть ли в файле пустые значения
+# print(data.isnull().sum())
+# # 2. Показать median_house_value где median_income < 2
+# res = data[data['median_income'] < 2]['median_house_value']
+# print(res)
+# 3. Показать данные в первых 2 столбцах
+# res = data[['longitude', 'latitude']]
+# print(res)
+# res = data.iloc[:, :2]          #получение 2-х стлбцов через срезы, 1: - все строки; 2: - только первые 2 слобца
+# print(res)
+# 4. Выбрать данные где housing_median_age < 20 и  median_house_value > 70000
+res = data[(data['housing_median_age'] < 20) & (data['median_house_value'] > 70_000)]
+print(res)
+
+
+
+'''
+Задача №61. Решение в группах
+1. Определить какое максимальное и минимальное
+значение median_house_value
+2. Показать максимальное median_house_value, где
+median_income = 3.1250
+3. Узнать какая максимальная population в зоне
+минимального значения median_house_value
+'''
+
+from pandas import read_csv
+data = read_csv('california_housing_test.csv')
+
+# 1. Определить какое максимальное и минимальное значение median_house_value
+# print(f"{data['median_house_value'].max()}, {data['median_house_value'].min()}")
+
+# # 2. Показать максимальное median_house_value, где median_income = 3.1250
+# print(data[data['median_income'] == 3.1250]['median_house_value'].max())
+
+# # 3. Узнать какая максимальная population в зоне минимального значения median_house_value
+res = data[data['median_house_value'] == data['median_house_value'].min()]['population'].max()
+print(res)
+
